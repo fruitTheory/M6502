73131dfa4bf1812861c294d4cf97eb3312d5143e
Derwin <derwinprince61@gmail.com>
2023-06-30 00:58:51 -0400

    changes

---
+++ src/MOS_6502_stack.c
@@ -0,0 +1,18 @@
+#include "MOS_6502_stack.h"
+#include "MOS_6502.h"
+#include <assert.h>
+
+
+static void MOS_6502_stack_inbounds(struct MOS_6502* MOS_6502){
+    assert(MOS_6502->registers.SP < stack_max_size);
+}
+
+void MOS_6502_stack_push(struct MOS_6502* MOS_6502){
+    MOS_6502->registers.SP--;
+    printf("%i\n", MOS_6502->registers.SP);
+}
+
+void MOS_6502_stack_pop(struct MOS_6502* MOS_6502){
+    MOS_6502->registers.SP++;
+    printf("%i\n", MOS_6502->registers.SP);
+}
